be4f4a35a9abc2829184bafadc356d021e22bed5
Alexandre Pauly <alexandrepauly775@gmail.com>
2024-09-15 11:55:09 +0200

    FIX hide content on mobile devices

--- js/onloadFunction.js
+++ js/onloadFunction.js
@@ -66,4 +66,6 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementsByClassName("mobile-hide")[0].style.display = "none";
     document.getElementsByClassName("mobile-message")[0].style.display = "block";
   }
+
+  document.getElementsByClassName("test")[0].innerHTML = navigator.userAgent;
 });
